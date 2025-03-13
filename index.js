88366c3ac93c41e96e09addbc2cc84d583e12d0e
Krishna27Singh <krishnasingh97857@gmail.com>
2025-03-13 13:14:40 +0530

    blog

---
+++ index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { checkForAuthenticationCookie } = require('./middlewares/authentication1');
+
+const Blog = require('./models/blog');
+
+const userRoute = require('./routes/user1');
+const blogRoute = require('./routes/blog');
+
+const app = express();
+const PORT = 8002;
+
+mongoose.connect('mongodb://localhost:27017/EchoSphere')
+    .then(e => console.log("MongoDB Connected!"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+    const allBlogs = await Blog.find({});
+    //desending order m sort kr rahe h
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    });
+})
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`))
